4827c0c6297a5eb85a54c3abf0d066f4fd2d8dc8
Ed Melech <mr.melech@gmail.com>
2021-06-14 15:41:25 +0100

    create accordion component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Accordion from './components/Accordion ';
 
 export default () => {
-  return <h1>Widget App</h1>;
+  return (
+    <div>
+      <Accordion />
+    </div>
+    )
 };
