f91ab8dcec1507fd9669a926b88fb89a7a4f5fe5
Ed Melech <mr.melech@gmail.com>
2021-06-14 16:52:28 +0100

    add helper funtion inside function component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const Accordion = ({ items }) => {
+  const onTitleClick = (index) => {
+    console.log('Title Clicked', index);
+  };
+
+
+
   const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
         <div
           className="title active"
-          onClick ={() => console.log('Title clicked', index)}
+          onClick ={() => onTitleClick(index)}
         >
           <i className="dropdown icon"></i>
           {item.title}
