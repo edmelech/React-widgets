58cd2d69758266b83d52ed33ff61e80fe6d1968a
Ed Melech <mr.melech@gmail.com>
2021-06-15 17:00:47 +0100

    import Search from components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Accordion from './components/Accordion';
-import Search from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
  {
@@ -20,8 +20,7 @@ const items = [
 export default () => {
   return (
     <div>
-      <br />
-      <Accordion items={items} />
+      <Search />
     </div>
-  )
+  );
 };
