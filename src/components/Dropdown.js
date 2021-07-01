b9d1907cab64a2ebcc183c8e35275fca41441279
Ed Melech <mr.melech@gmail.com>
2021-07-01 21:29:05 +0100

    use useRef hook

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,12 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
 
   useEffect(() => {
-    document.body.addEventListener('click', () => {
-      console.log('CLICK!!!');
-    });
+    document.body.addEventListener(
+      'click',
+      (event) => {
+        if (ref.current.contains(event.target)) {
+          return;
+        }
+
+        setOpen(false);
+      },
+      { capture: true }
+    );
   }, []);
 
   const renderedOptions = options.map((option) => {
@@ -19,20 +28,23 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       <div
         key={option.value}
         className="item"
-        onClick={() => onSelectedChange(option)}
+        onClick={() => {
+          onSelectedChange(option)
+        }}
       >
         {option.label}
       </div>
     );
   });
 
-
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
         <div
-          onClick={()=> setOpen(!open)}
+          onClick={() => {
+            setOpen(!open);
+          }}
           className={`ui selection dropdown ${open ? 'visible active' : ''}`}
         >
           <i className="dropdown icon"></i>
