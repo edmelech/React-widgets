25023da0efff5e9f0e8b1340d5114fb95bb702e4
Ed Melech <mr.melech@gmail.com>
2021-07-05 19:46:17 +0100

    add stlying to input form

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -22,7 +22,12 @@ const Translate = () => {
 
   return (
     <div>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
       <Dropdown
         label="Select a language"
         selected={language}
