6f33c7cc748f2e5b890c9c9858ded0f7a8ce3446
Ed Melech <mr.melech@gmail.com>
2021-07-12 19:11:34 +0100

    create a use effect function and define timer

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -6,6 +6,16 @@ const Convert = ({ language, text }) => {
   const [debouncedText, setDebouncedText] = useState(text);
 
   useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+
+  useEffect(() => {
     const doTranslation = async () => {
       const { data } = await axios.post(
         'https://translation.googleapis.com/language/translate/v2',
